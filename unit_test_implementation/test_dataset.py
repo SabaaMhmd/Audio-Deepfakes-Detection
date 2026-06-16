4695af0888e1d0045e2536bed0e4beb93d888978
SabaaMhmd <167319757+SabaaMhmd@users.noreply.github.com>
2026-06-16 18:51:22 +0300

    Create test_dataset.py

---
+++ unit_test_implementation/test_dataset.py
@@ -0,0 +1,24 @@
+from software_implementation.aasist.data_utils import Dataset_ASVspoof2019_train
+
+
+def test_dataset_length():
+
+    dummy_files = [
+        "audio1.wav",
+        "audio2.wav",
+        "audio3.wav"
+    ]
+
+    labels = {
+        "audio1.wav": 0,
+        "audio2.wav": 1,
+        "audio3.wav": 0
+    }
+
+    dataset = Dataset_ASVspoof2019_train(
+        dummy_files,
+        labels,
+        None
+    )
+
+    assert len(dataset) == 3
