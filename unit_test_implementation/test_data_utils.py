f3a4eba8f0c58cfc218fd458b2d86b022ca37f5b
SabaaMhmd <167319757+SabaaMhmd@users.noreply.github.com>
2026-06-16 19:04:40 +0300

    Update test_data_utils.py

--- unit_test_implementation/test_data_utils.py
+++ unit_test_implementation/test_data_utils.py
@@ -1,28 +1,23 @@
 import sys
 sys.path.append("software_implementation/aasist")
 
+import numpy as np
 from data_utils import pad, pad_random
-from software_implementation.aasist.data_utils import (
-    pad,
-    pad_random
-)
+
 
 def test_pad_output_length():
     x = np.random.randn(1000)
     output = pad(x, max_len=96000)
-
     assert len(output) == 96000
 
 
 def test_pad_random_output_length():
     x = np.random.randn(5000)
     output = pad_random(x, max_len=96000)
-
     assert len(output) == 96000
 
 
 def test_pad_keeps_exact_length():
     x = np.random.randn(96000)
     output = pad(x, max_len=96000)
-
     assert len(output) == 96000
